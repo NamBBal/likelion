c7843c3f9842be54d52b3db1055ab4ab781764e4
Nambbal <chlgksqlscjswo12>
2023-04-19 11:37:01 +0900

    feat : Bye기능 추가 - ByeClass, ByeClassTest 추가

---
+++ src/main/java/com/example/javaproject2/ByeClassTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2;
+
+public class ByeClassTest {
+    public static void main(String[] args) {
+        ByeClass byeClass = new ByeClass(); // 인스턴스화
+        byeClass.print();
+    }
+}
