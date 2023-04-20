6b4dfcca689b1b68dac04aa213ef4e9413bf1eec
Nambbal <chlgksqlscjswo12>
2023-04-20 17:47:35 +0900

    feat: 코드업 1012~1015 복습 완료 :)

--- src/main/java/codeup/CodeUp1012.java
+++ src/main/java/codeup/CodeUp1012.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class CodeUp1012 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.printf("%f", sc.nextFloat());
+        System.out.printf("%d %d",sc.nextInt(),sc.nextInt());
     }
 }
--- src/main/java/codeup/CodeUp1014.java
+++ src/main/java/codeup/CodeUp1014.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class CodeUp1014 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String b = sc.next();
-        System.out.printf("%s %s", b, a);
+        String fw = sc.next();
+        String bw = sc.next();
+        System.out.printf("%s %s", bw, fw);
     }
 }
 
