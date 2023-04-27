b03e1cc68fb3a37b43da608eb4d62b18b7930663
Nambbal <chlgksqlscjswo12>
2023-04-27 17:37:50 +0900

    feat: 2주 4일차 수업내용 리뷰

---
+++ src/main/java/com/example/javaproject2/week2/DefineExercise.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week2;
+
+public class DefineExercise {
+    public static void main(String[] args) {
+        String sOne = new String("1");
+        int iOne = 1;
+        float fOne = 1.0f;
+    }
+}
